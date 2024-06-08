12124610a599e397500c18b19072e54fd96a0e48
Carlos <carlos.marcelo1992@gmail.com>
2024-06-08 15:44:38 +0200

    add confetis

--- src/components/screens/VictoryScreen/VictoryScreen.jsx
+++ src/components/screens/VictoryScreen/VictoryScreen.jsx
@@ -1,10 +1,12 @@
 import "./VictoryScreen.scss";
+import Fireworks from "react-canvas-confetti/dist/presets/fireworks";
 
 function VictoryScreen() {
   return (
     <div className="victory-screen">
       <h1>Congratulations!</h1>
       <p>You Won!</p>
+      <Fireworks autorun={{ speed: 3 }} />
     </div>
   );
 }
